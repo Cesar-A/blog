45eb79c65678e9c33d9c50821096c4c3932ed0f2
Cesar Arroyo <cesararroyo1234@gmail.com>
2014-11-05 11:58:22 -0800

    adding title bar

---
+++ view/form.php
@@ -0,0 +1,11 @@
+<h1>Create Blog post</h1>
+<form>
+    <div>
+        <label for="title">Title: </label>   
+        <input type="text" name="title"
+    </div>
+        <div>
+            <label for="post">post: </label>   
+            <textarea name="post"></textarea>      
+    </div>
+</form>
