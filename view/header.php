d37eb820a2cd16c02091770dab8bb4af831a9721
Cesar Arroyo <cesararroyo1234@gmail.com>
2014-11-07 10:47:54 -0800

    adding media queries

--- view/header.php
+++ view/header.php
@@ -7,7 +7,8 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-         <link rel="stylesheet" type="text/css" href="main.css">
+        <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+        <link rel="stylesheet" type="text/css" href="main.css">
         <title></title>
     </head>
     
