eac5d0ec64d932fc81b75089f7b922892d5512b6
Cesar Arroyo <cesararroyo1234@gmail.com>
2014-11-12 12:25:54 -0800

    connecting the controller to form , establishing a database connection

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,7 +1,9 @@
 <?php
-$title = filler_input(INPUT_POST,"title",FILTER_SANITIZE_STRING);//inputs title to user
+$title = filter_input(INPUT_POST,"title",FILTER_SANITIZE_STRING);//inputs title to user
 $post = filter_input(INPUT_POST,"post",FILTER_SANITIZE_STRING);//inputs post to user
 
 echo "<p> Title: $title</p>";//echos out title
 echo "<p> Post: $post</p>";//echos out post
+
+
 
