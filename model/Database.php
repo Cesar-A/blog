d35e445f1c6bcc1811c1cdfea00bf0c983852755
Cesar Arroyo <cesararroyo1234@gmail.com>
2014-11-17 13:18:47 -0800

    video 27: creating a constructor for a database class video 28: creating functions within our database class

--- model/Database.php
+++ model/Database.php
@@ -1,9 +1,25 @@
 <?php
 class Database{
-    private $connection;
-    private $host;
-    private $username;
+    private $connection; //created an instance of variables 
+    private $host;  //these will be called around the database as an object
+    private $username;//and can store information and be called multiple times
     private $password ;
     private $database;
+    
+    public function __construct($host, $username, $password, $database){
+        $this->host = $host;
+        $this->host = $username;
+        $this->host = $password;
+        $this->host = $database;    
+    }
+    public function openConnection () {
+    }
+    public function closeConnection () {
+        
+    }
+    public function query ($string) {
+          
+      }
+    
 }
 
