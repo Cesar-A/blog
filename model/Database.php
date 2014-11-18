36867ae2cfa41627d1a4bd841e87d63241011b30
Cesar Arroyo <cesararroyo1234@gmail.com>
2014-11-18 12:10:01 -0800

    video 29: implemented the open Connection function to the blog

--- model/Database.php
+++ model/Database.php
@@ -1,27 +1,39 @@
 <?php
-class Database{
+
+class Database {
+
     private $connection; //created an instance of variables 
     private $host;  //these will be called around the database as an object
-    private $username;//and can store information and be called multiple times
-    private $password ;
+    private $username; //and can store information and be called multiple times
+    private $password;
     private $database;
-    
-    public function __construct($host, $username, $password, $database){ //created a public function which allows 
+
+    public function __construct($host, $username, $password, $database) { //created a public function which allows 
+        
         $this->host = $host;                                               // the user to access the information 
         $this->host = $username;
         $this->host = $password;
-        $this->host = $database;    
+        $this->host = $database;
     }
-    public function openConnection () {
-    }                                   //__construct() is the name for the constructor. 
-                                        //The constructor becomes a object after it has been created
-                                         //it then can be used to store code
-    public function closeConnection () {
+
+    public function openConnection() {
+        $this->connection = new mysqli($this->$host, $this->username, $this->password, $this->databaase);
+        
+        if ($this->$connection->connect_error) {
+            die("Error: " . $this->connection->connection_error);
+        }
+    }
+
+//__construct() is the name for the constructor. 
+    //The constructor becomes a object after it has been created
+    //it then can be used to store code
+
+    public function closeConnection() {
         
     }
-    public function query ($string) {
-          
-      }
-    
-}
 
+    public function query($string) {
+        
+    }
+
+}
