f28255aaf2513f3a9fb75345c31a5b7a8e9d1a36
Cesar Arroyo <cesararroyo1234@gmail.com>
2014-12-04 12:14:33 -0800

    video38: Database table to store registered users

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once(__DIR__ . "/../model/config.php");
 
 
@@ -14,3 +15,18 @@ if ($query) {
 } else {
     echo "<p>" . $_SESSION["connection"]->error . "</p>";
 }
+
+$query = $_SESSION["connection"]->query("CREATE TABLE users("
+        . "id int(11)NOT NULL AUTO_INCREMENT,"
+        . "username varchar(30) NOT NULL,"
+        . "email  varchar(50) NOT NULL,"
+        . "password char(128) NOT NULL,"
+        . "salt char(128) NOT NULL,"
+        . "PRIMARY KEY(id))");
+
+if ($query) {
+    echo"<p> Succesfully created table: users</p>";
+} else {
+    echo"<p>" . $_SESSION["connection"]->error . "</p>";
+}
+
